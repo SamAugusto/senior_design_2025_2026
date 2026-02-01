484d26204da85446fc1efa9dfddb2cfe01f2d6b6
SamAugusto <SamAugusto121@gmail.com>
2026-02-01 03:34:23 -0500

    removing some comments polishing the script

--- scripts/main.py
+++ scripts/main.py
@@ -179,13 +179,8 @@ def main():
 
 
 
-    # % of aligment molecule to contig or reference idk how to do this yet?
 
-
-    # account for orientation
-    # include distance to decide in the molecular bins
-    # count each label(row past the distance)
-    ## Ex moleculde_id#_(row_count,distance_to_end)
+    # account for orientation?
 
     # save it in a txt can change later
 
